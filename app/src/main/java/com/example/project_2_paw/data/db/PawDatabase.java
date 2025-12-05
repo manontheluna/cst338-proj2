5e63d8e9eb2147cfe7adcfe25717cf3d6473535d
q-siddiq <annierahimsiddiq@gmail.com>
2025-12-05 10:19:00 -0800

    Issue #4: Added PawDatabase

---
+++ app/src/main/java/com/example/project_2_paw/data/db/PawDatabase.java
@@ -0,0 +1,45 @@
+package com.example.project_2_paw.data.db;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.project_2_paw.data.dao.UserDAO;
+import com.example.project_2_paw.data.dao.PetDAO;
+import com.example.project_2_paw.data.entity.User;
+import com.example.project_2_paw.data.entity.Pet;
+
+
+/**
+ * Author: Quratulain Siddiq
+ * Project 2: P.A.W (Pet, Activity & Wellness)
+ * Main Room Database class for the P.A.W. application.
+ * Provides access to DAO objects for User and Pet data.
+ */
+
+@Database(entities = { User.class, Pet.class }, version = 1, exportSchema = false)
+public abstract class PawDatabase extends RoomDatabase{
+    // DAO accessors
+    public abstract UserDAO userDAO();
+    public abstract PetDAO petDAO();
+
+    // Singleton instance of the database
+    private static volatile PawDatabase INSTANCE;
+
+    public static PawDatabase getInstance(Context context) {
+        if (INSTANCE == null) {
+            synchronized (PawDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(
+                            context.getApplicationContext(),
+                            PawDatabase.class,
+                            "paw_database"
+                    ).build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
