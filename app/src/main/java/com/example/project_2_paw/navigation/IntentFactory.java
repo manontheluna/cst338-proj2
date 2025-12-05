ae706aa7d408b03838bd9486f3a1544f299ca5f1
manontheluna <eluna-ortez@csumb.edu>
2025-12-04 20:07:05 -0800

    add more relevant intents to intent factory; important to note, some of these classes dont exist currently

--- app/src/main/java/com/example/project_2_paw/navigation/IntentFactory.java
+++ app/src/main/java/com/example/project_2_paw/navigation/IntentFactory.java
@@ -3,12 +3,22 @@ package com.example.project_2_paw.navigation;
 import android.content.Context;
 import android.content.Intent;
 
+import com.example.project_2_paw.LoginView;
+
 public class IntentFactory {
     public static Intent login(Context context) {
         return new Intent(context, LoginView.class);
     }
 
     public static Intent signup(Context context) {
-        return new Intent(context, SignUp.class);
+        return new Intent(context, SignUpView.class);
+    }
+
+    public static Intent dashboard(Context context) {
+        return new Intent(context, Dashboard.class);
+    }
+
+    public static Intent petView(Context context) {
+        return new Intent(context, PetView.class);
     }
 }
