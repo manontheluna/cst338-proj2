3f893e871f45b495e07dcd78830eb0c5f19e3b88
manontheluna <eluna-ortez@csumb.edu>
2025-12-07 18:29:51 -0800

    display username to user on login

--- app/src/main/java/com/example/project_2_paw/MainActivity.java
+++ app/src/main/java/com/example/project_2_paw/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.project_2_paw;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +11,19 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView welcome;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        welcome = findViewById(R.id.welcome);
+
+        String username = getIntent().getStringExtra("username");
+
+        if (username != null) {
+            welcome.setText("Welcome, " + username + "!");
+        }
     }
 }
