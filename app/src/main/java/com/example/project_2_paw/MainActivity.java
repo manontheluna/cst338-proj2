f488ed20f42855f79e40c35371f232e35ce5dbbb
manontheluna <eluna-ortez@csumb.edu>
2025-12-07 18:49:59 -0800

    add view structure for displaying list of pets and pet creation for logged in user

--- app/src/main/java/com/example/project_2_paw/MainActivity.java
+++ app/src/main/java/com/example/project_2_paw/MainActivity.java
@@ -9,6 +9,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView welcome;
