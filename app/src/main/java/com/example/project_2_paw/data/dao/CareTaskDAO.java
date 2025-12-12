90afda2755f3d37c7eba242372820fc41e1cc078
q-siddiq <annierahimsiddiq@gmail.com>
2025-12-12 14:45:47 -0800

    Added AdminTaskAdapter and Added CareTask List in CareTask

--- app/src/main/java/com/example/project_2_paw/data/dao/CareTaskDAO.java
+++ app/src/main/java/com/example/project_2_paw/data/dao/CareTaskDAO.java
@@ -24,4 +24,7 @@ public interface CareTaskDAO {
 
     @Query("SELECT * FROM caretask WHERE petId = :petId")
     LiveData<List<CareTask>> getTasksForPet(int petId);
+
+    @Query("SELECT * FROM CareTask")
+    List<CareTask> getAllTasks();
 }
