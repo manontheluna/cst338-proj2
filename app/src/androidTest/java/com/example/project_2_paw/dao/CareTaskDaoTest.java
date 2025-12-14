d777ae0c04ea491e50c146f4e94b49a3e8069da4
Manuel Caro <mcaro@csumb.edu>
2025-12-14 13:01:36 -0800

    Add insert test for CareTaskDAO

--- app/src/androidTest/java/com/example/project_2_paw/dao/CareTaskDaoTest.java
+++ app/src/androidTest/java/com/example/project_2_paw/dao/CareTaskDaoTest.java
@@ -6,6 +6,10 @@ package com.example.project_2_paw.dao;
  * @description Unit test for CareTaskDao
  */
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
 import android.app.Application;
 import android.content.Context;
 
@@ -15,11 +19,16 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.example.project_2_paw.data.dao.CareTaskDAO;
 import com.example.project_2_paw.data.db.PawDatabase;
+import com.example.project_2_paw.data.entity.CareTask;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runner.manipulation.Ordering;
+
+import java.time.LocalDate;
+import java.util.List;
 
 /**
  * Unit test for CareTaskDAO
@@ -32,16 +41,31 @@ public class CareTaskDaoTest {
     private CareTaskDAO careTaskDao;
 
     @Before
-    public void setup(){
+    public void setup() {
         Context context = ApplicationProvider.getApplicationContext();
         db = Room.inMemoryDatabaseBuilder(context, PawDatabase.class)
                 .allowMainThreadQueries()
                 .build();
         careTaskDao = db.careTaskDAO();
     }
+
     @After
-    public void teardown(){
+    public void teardown() {
         db.close();
     }
 
+    @Test
+    public void insertTask_savesTaskToDatabase() {
+        CareTask task = new CareTask(1, "Walk", LocalDate.of(2025, 5, 29), false);
+        careTaskDao.insertTask(task);
+        List<CareTask> tasks = careTaskDao.getTasksForPetSync(1);
+        assertNotNull(tasks);
+        assertEquals(1, tasks.size());
+
+        CareTask saved = tasks.get(0);
+        assertEquals(1, saved.getPetId());
+        assertEquals("Walk", saved.getTaskName());
+        assertEquals(LocalDate.of(2025, 5, 29), saved.getDueDate());
+        assertFalse(saved.isCompleted());
+    }
 }
