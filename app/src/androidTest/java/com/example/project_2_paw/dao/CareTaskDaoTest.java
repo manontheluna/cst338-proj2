09511db92fe59b107981fb6ff83061fff3b42365
Manuel Caro <mcaro@csumb.edu>
2025-12-14 12:33:01 -0800

    Initial commit for CareTaskDaoTest

---
+++ app/src/androidTest/java/com/example/project_2_paw/dao/CareTaskDaoTest.java
@@ -0,0 +1,14 @@
+package com.example.project_2_paw.dao;
+
+/**
+ * @author Manuel Caro
+ * @date 12/14/2025
+ * @description Unit test for CareTaskDao
+ */
+
+/**
+ * Unit test for CareTaskDAO
+ * Verifies basic operations: insert, update, and delete.
+ */
+public class CareTaskDaoTest {
+}
