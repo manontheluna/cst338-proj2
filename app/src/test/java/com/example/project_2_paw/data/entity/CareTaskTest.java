600b97e57e705b044ffb2f90c0924890b4bd1d07
Manuel Caro <mcaro@csumb.edu>
2025-12-13 20:54:29 -0800

    CareTaskTest Initial Commit

---
+++ app/src/test/java/com/example/project_2_paw/data/entity/CareTaskTest.java
@@ -0,0 +1,58 @@
+package com.example.project_2_paw.data.entity;
+
+/**
+ * @author Manuel Caro
+ * @date 12/13/2025
+ */
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit tests for the CareTask entity.
+ * Verifies that getters and setters behave as expected.
+ */
+
+public class CareTaskTest {
+
+    @Test
+    public void getTaskId() {
+    }
+
+    @Test
+    public void setTaskId() {
+    }
+
+    @Test
+    public void getPetId() {
+    }
+
+    @Test
+    public void setPetId() {
+    }
+
+    @Test
+    public void getTaskName() {
+    }
+
+    @Test
+    public void setTaskName() {
+    }
+
+    @Test
+    public void getDueDate() {
+    }
+
+    @Test
+    public void setDueDate() {
+    }
+
+    @Test
+    public void isCompleted() {
+    }
+
+    @Test
+    public void setCompleted() {
+    }
+}
